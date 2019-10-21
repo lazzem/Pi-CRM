5554616b0e4b1116e36b4bf8197f7f77fa6caf91
lazzem <mahdi.lazzem@gmail.com>
2019-10-21 21:26:31 +0200

    First Try

---
+++ Data/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,28 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Infrastructure
+{
+    public class DatabaseFactory : Disposable, IDatabaseFactory
+    {
+        private MyFinanceContexte dataContext;
+        public MyFinanceContexte DataContext { get { return dataContext; } }
+
+        public DatabaseFactory()
+        {
+            dataContext = new MyFinanceContexte();
+        }
+
+        protected override void DisposeCore()
+        {
+            // libérer espace mémoire du context
+            if(DataContext!=null)
+            DataContext.Dispose();
+        }
+    }
+
+}
